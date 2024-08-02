16ab941fe4e30acd61d493fbca63e3482d313292
Mehar Zain <zainzainpubg1122@gmail.com>
2024-08-02 20:58:35 +0500

    p

--- oka_project/urls.py
+++ oka_project/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+from django.conf import settings
 from django.views.static import serve
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
