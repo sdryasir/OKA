8ece1266c4915d28433dc4d8696c2b8d433140f6
Mehar Zain <zainzainpubg1122@gmail.com>
2024-08-02 21:00:52 +0500

    p

--- oka_project/urls.py
+++ oka_project/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
-from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.static import serve
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
@@ -47,3 +46,5 @@ urlpatterns = [
     path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
+if settings.DEBUG :
+    urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
