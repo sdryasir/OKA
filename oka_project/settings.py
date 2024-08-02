88ee3587eef729efa0fc938ca1955cd7cc8bb5e7
Mehar Zain <zainzainpubg1122@gmail.com>
2024-08-02 20:54:51 +0500

    push

--- oka_project/settings.py
+++ oka_project/settings.py
@@ -136,6 +136,7 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
