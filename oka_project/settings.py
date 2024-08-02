5a01995ff506c17f3ab9f5818445c860c12e4bf1
Mehar Zain <zainzainpubg1122@gmail.com>
2024-08-02 14:52:49 +0500

    t

--- oka_project/settings.py
+++ oka_project/settings.py
@@ -138,13 +138,6 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
-
 
 
 
