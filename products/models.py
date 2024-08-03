eb48292c19c6da50f33c7e5e977be1e99d4879b7
muhammadubaid786 <muhammadubaid007788@gmail.com>
2024-08-03 22:30:31 +0500

    adjusting

--- products/models.py
+++ products/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from categories.models import Category
 
 class Products(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.IntegerField() 
-    compare_price = models.IntegerField()
-    More_information = models.TextField(blank=True)
+    title = models.CharField(max_length=50 , null=True)
+    description = models.TextField(null=True)
+    price_that_you_sell = models.IntegerField(help_text='the price that you sell' , null=True) 
+    Orignal_price = models.IntegerField(help_text='this price is to attract the costumer and this is higher than price that you sell' , null=True)
+    More_information = models.TextField(blank=True , null=True)
     In_Stock = models.IntegerField(null=True)
-    category = models.ForeignKey(Category , on_delete=models.CASCADE)
-    image = models.FileField(max_length=60 , upload_to= "products/")
+    category = models.ForeignKey(Category , on_delete=models.CASCADE , null=True)
+    image = models.FileField(max_length=60 , upload_to= "products/" , null=True)
+
  
 
     def __str__(self):
