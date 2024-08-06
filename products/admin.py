1fd967602089340e17a4bb898386c3f4ab2ba7e0
Mehar Zain <zainzainpubg1122@gmail.com>
2024-08-06 08:11:41 +0500

    t

--- products/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Products
-class productAdmin(admin.ModelAdmin):
-    list_display = ['title' , 'price_that_you_sell' , 'availability' , 'category']
-
-admin.site.register(Products , productAdmin)
