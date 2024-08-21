3dcb68f1f0509a23a1d01bd4a4327ddd67d205ed
Mehar Zain <zainzainpubg1122@gmail.com>
2024-08-21 13:18:21 +0500

    ff

--- oka_project/views.py
+++ oka_project/views.py
@@ -23,7 +23,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
 
-@login_required(login_url="/users/login")
+@login_required(login_url="/login")
 def cart_add(request, id):
     if request.user.is_authenticated:
         cart = Cart(request)
@@ -36,35 +36,35 @@ def cart_add(request, id):
         cart.add(product=product)
         return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
-@login_required(login_url="/users/login")
+@login_required(login_url="/login")
 def item_clear(request, id):
     cart = Cart(request)
     product = Products.objects.get(id=id)
     cart.remove(product)
     return redirect("cart_detail")
 
-@login_required(login_url="/users/login")
+@login_required(login_url="/login")
 def item_increment(request, id):
     cart = Cart(request)
     product = Products.objects.get(id=id)
     cart.add(product=product)
     return redirect("cart_detail")
 
-@login_required(login_url="/users/login")
+@login_required(login_url="/login")
 def item_decrement(request, id):
     cart = Cart(request)
     product = Products.objects.get(id=id)
     cart.decrement(product=product)
     return redirect("cart_detail")
 
-@login_required(login_url="/users/login")
+@login_required(login_url="/login")
 def cart_clear(request):
     cart = Cart(request)
     cart.clear()
     return redirect("cart_detail")
 
 
-@login_required(login_url="/users/login")
+@login_required(login_url="/login")
 def cart_detail(request):
     cart = Cart(request)
     subtotal = 0
