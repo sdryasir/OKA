5d335c917fc4c6f8ce46cc7286fb7b4d13597d2d
Mehar Zain <zainzainpubg1122@gmail.com>
2024-08-31 13:44:26 +0500

    modified

--- categories/models.py
+++ categories/models.py
@@ -2,5 +2,7 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=20)
     image = models.FileField(max_length=60 , upload_to= "products/" , help_text='Image size must be 500 X 500 pixels')
+    class Meta:
+        verbose_name_plural = "Categories"
     def __str__(self):
         return self.title
