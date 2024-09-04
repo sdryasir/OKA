0cf4bdd0241860618e84c4baaea168b59f3a5456
muhammadubaid786 <muhammadubaid007788@gmail.com>
2024-09-04 13:01:24 +0500

    add user profile

--- users/forms.py
+++ users/forms.py
@@ -10,22 +10,29 @@ class CustomUserForm(UserCreationForm):
     state = forms.CharField(max_length=30, required=False)
     zip_code = forms.CharField(max_length=10, required=False)
     phone_no = forms.CharField(max_length=18, required=False)
+    profile_picture = forms.ImageField(required=False)
 
     class Meta:
         model = User
-        fields = [ "username", "email", "first_name", "last_name", "password1", "password2", 
-                "country", "address", "city", "state", "zip_code", "phone_no"]
-        exclude = ['last_login'] 
+        fields = [
+            "username", "email", "first_name", "last_name", "password1", "password2",
+            "country", "address", "city", "state", "zip_code", "phone_no", "profile_picture"
+        ]
 
-    def save(self):
-        user = super().save()
-        Userdata.objects.create(
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+        userdata, created = Userdata.objects.update_or_create(
             user=user,
-            country=self.cleaned_data.get('country'),
-            address=self.cleaned_data.get('address'),
-            city=self.cleaned_data.get('city'),
-            state=self.cleaned_data.get('state'),
-            zip_code=self.cleaned_data.get('zip_code'),
-            phone_no=self.cleaned_data.get('phone_no'),
-            )
+            defaults={
+                'country': self.cleaned_data.get('country'),
+                'address': self.cleaned_data.get('address'),
+                'city': self.cleaned_data.get('city'),
+                'state': self.cleaned_data.get('state'),
+                'zip_code': self.cleaned_data.get('zip_code'),
+                'phone_no': self.cleaned_data.get('phone_no'),
+                'profile_picture': self.files.get('profile_picture')
+            }
+        )
         return user
--- users/models.py
+++ users/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 class Userdata(models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
-    country = models.CharField(max_length=100 , null=True)
-    address = models.CharField(max_length=50 , null=True)
+    country = models.CharField(max_length=50 , null=True)
+    address = models.CharField(max_length=100 , null=True)
     city = models.CharField(max_length=50 , null=True)
     state = models.CharField(max_length=50 , null=True)
     zip_code = models.CharField(max_length=50 , null=True)
     phone_no = models.CharField(max_length=18 , null=True)
+    profile_picture = models.ImageField(upload_to='products/', null=True, blank=True)
+
